5267e247102ec63089937b6656ac5237285ac8c8
joel_ <jojostev@gmail.com>
2019-11-07 23:03:21 +0400

    Commit 6 : Corrections made to make the program PEP-8 Compliant.

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import requests
 import sqlite3
-from ratelimit import limits,sleep_and_retry,RateLimitException
-from backoff import on_exception,expo
+from ratelimit import limits, sleep_and_retry, RateLimitException
+from backoff import on_exception, expo
+
 
 '''
 System Necessary Variables
 '''
 FIVE_MINUTES = 300
 
-connection = sqlite3.connect('database.db') #connect to database
-cursor = connection.cursor() #instantiate the cursor object
+connection = sqlite3.connect('database.db')  # connect to database
+cursor = connection.cursor()  # instantiate the cursor object
 
 
 '''
 Definitions
 '''
+
+
 @sleep_and_retry
 @on_exception(expo, RateLimitException, max_tries=300)
 @limits(calls=300, period=FIVE_MINUTES)
 def call_api():
-    '''
+    """
     Rules for 300 requests in 5 mins has been implemented using the ratelimit module.
     This function will call the ReST API and read the data.
     If the
 
     :return: data which is a JSON object.
-    '''
+    """
     url = 'https://jsonplaceholder.typicode.com/comments'
     resp = requests.get(url=url)
     data = resp.json()
@@ -35,23 +38,23 @@ def call_api():
     return data
 
 
-
 def drop_table():
-    '''
+    """
     If table exists, Drop table.
 
     :return:
-    '''
-    cursor.execute ("DROP TABLE IF EXISTS data_table")
+    """
+    cursor.execute("DROP TABLE IF EXISTS data_table")
     connection.commit()
 
+
 def create_table():
-    '''
+    """
     If table does not exist, Table is create.
     If exists, return error.
     Error is caught in try/except loop.
     :return:
-    '''
+    """
     sql_command = """
         CREATE TABLE IF NOT EXISTS data_table (
                             postId INTEGER,
@@ -59,8 +62,6 @@ def create_table():
                             name VARCHAR(1000),
                             email VARCHAR(200),
                             body CHAR(1000));"""
-
-
 
     try:
         cursor.execute(sql_command)
@@ -70,7 +71,7 @@ def create_table():
 
 
 def write_data_to_table():
-    '''
+    """
     Instruction passed to create table.
     call_api() function is called and data variable accepts data.JSON object.
 
@@ -78,7 +79,7 @@ def write_data_to_table():
 
     When all data has been entered a message is flashed to show the number of data points inserted to table.
     :return:
-    '''
+    """
     create_table()
     data = call_api()
 
@@ -94,7 +95,7 @@ def write_data_to_table():
     
                         ) 
                         VALUES ({postId}, {id}, "{name}", "{email}", "{body}");"""
-        sql_command = format_str.format(postId=i['postId'],id=i['id'],name=i['name'],email=i['email'],body=a)
+        sql_command = format_str.format(postId=i['postId'], id=i['id'], name=i['name'], email=i['email'], body=a)
         cursor.execute(sql_command)
         if i['id'] == len(data):
             print(f"{i['id']} data fields have been entered into table from the ReST API.")
@@ -104,18 +105,17 @@ def write_data_to_table():
 
 
 def runnable():
-    '''
+    """
     This definition is to provide a concise running enviroment to enhance usability only.
     functions are called per user instructions making the user experience optimal.
     :return:
-    '''
+    """
     print('Hello, Welcome to the Database. ')
     print('''
     Your options are :
     1. Write DATA from https://jsonplaceholder.typicode.com/comments
     2. Delete Existing Data from Table 
     3. Exit''')
-
 
     def options():
         option_input = int(input("Enter Option: "))
@@ -128,7 +128,7 @@ def runnable():
                 print('''Oops, Looks like data already exists. Enter option 2 and try again. ''')
                 print('\n')
                 options()
-        elif option_input == 2 :
+        elif option_input == 2:
             drop_table()
             print('Existing table has been dropped successfully!')
             create_table()
@@ -137,7 +137,7 @@ def runnable():
             options()
         elif option_input == 3:
             print('Thank you for using the Program. Exiting Now.')
-            exit
+
         else:
             print('Invalid Option Choice, please enter the valid choice again.')
             print('\n')
